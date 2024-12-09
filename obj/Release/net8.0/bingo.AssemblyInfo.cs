eb5d750947c2cf399c4b9639c1d15a8b502b8838
Mikel <mikel.kunze887@web.de>
2024-12-09 19:50:53 +0100

    No same words | Css changes

--- obj/Release/net8.0/bingo.AssemblyInfo.cs
+++ obj/Release/net8.0/bingo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bingo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+349deaa2d2756d4c8276dcf83c13a67cf94bb9d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("bingo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bingo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
