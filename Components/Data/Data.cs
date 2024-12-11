017b6bf5e2cd24a23a0629f4786ea237184cebad
Mikel <mikel.kunze887@web.de>
2024-12-11 20:08:54 +0100

    Bingo finished

--- Components/Data/Data.cs
+++ Components/Data/Data.cs
@@ -3,3 +3,11 @@ namespace Data;
 public class Bingodata {
     public List<string>? bingoWords { get; set; }
 }
+
+public class Bingorows {
+    public int[] bingorow1 = {0, 5, 10, 15, 20};
+    public int[] bingorow2 = {1, 6, 11, 16, 21};
+    public int[] bingorow3 = {2, 7, 12, 17, 22};
+    public int[] bingorow4 = {3, 8, 13, 18, 23};
+    public int[] bingorow5 = {4, 9, 14, 19, 24};
+}
