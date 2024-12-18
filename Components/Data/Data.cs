ad92156d868753a3eb50c612e9ed63cbe3b7aa60
Mikel <mikel.kunze887@web.de>
2024-12-18 21:21:46 +0100

    Added diagonal bingo

--- Components/Data/Data.cs
+++ Components/Data/Data.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Components.Forms;
+
 namespace Data;
 
 public class Bingodata {
@@ -10,6 +12,9 @@ public class Bingorows {
     public int[] bingorow3 = {2, 7, 12, 17, 22};
     public int[] bingorow4 = {3, 8, 13, 18, 23};
     public int[] bingorow5 = {4, 9, 14, 19, 24};
+
+    public int[] bingorowside1 = {0, 6, 12, 18, 24};
+    public int[] bignorowside2 = {4, 8, 12, 16, 20};
 }
 
 public class NewBingo {
--- obj/Release/net8.0/bingo.AssemblyInfo.cs
+++ obj/Release/net8.0/bingo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bingo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c393e18e75619801cc405da996d4c9e09eb35f3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3912bf378474322d15f8fde935b95431d387ae12")]
 [assembly: System.Reflection.AssemblyProductAttribute("bingo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bingo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
